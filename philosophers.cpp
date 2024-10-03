717eae208fb2e9639401caf501a6d685119663c6
Nickson1 <moople256@gmail.com>
2024-10-03 16:39:09 -0400

    First commit. Some preliminary work on the code.

---
+++ philosophers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class philosopher {
+	private:
+		int hungerMeter = 100;
+	public:
+		int getHunger (void);
+		void eat (void); //will fill hunger meter a set amount
+	
+};
+
+int main (void) {
+	
+	return 0;
+}
